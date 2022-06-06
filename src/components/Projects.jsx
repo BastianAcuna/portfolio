b31a546465ae0554714594e61e336dc369c4f5db
BastianAcuna <BastianAcS@hotmail.com>
2022-06-06 09:00:46 -0400

    portfolioo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -54,7 +54,7 @@ const Projects = (props) => {
                   variant={theme.bsSecondaryVariant}
                   onClick={() => setShowMore(true)}
                 >
-                  show more
+                  .
                 </Button>
                 )}
             </Container>
